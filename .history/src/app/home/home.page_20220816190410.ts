1627aee9b14ac17115b84259cdd7800074b7dfaa
Max N <smth@gmail.com>
2022-08-16 19:06:36 +0700

    home page sections removed

---
+++ .history/src/app/home/home.page_20220816190410.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ShoppingCartPage } from '../shopping-cart/shopping-cart.page';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public router: Router) {}
+
+  gotToShopping(){
+    this.router.navigate(['/shopping-cart']);
+  }
+  gotToResults(){
+    this.router.navigate(['/results']);
+  }
+}
