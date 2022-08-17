268fbe0020c9f3a577f7b0f3fe92dfb6e502b4ba
James Hickman <jvhickman@myseneca.ca>
2022-08-17 01:59:51 -0400

    fix

---
+++ src/app/cart.model.ts
@@ -0,0 +1,15 @@
+export class Cart {
+    title: string
+    price: string
+    image: string
+    link: string
+
+
+    constructor(t: string, p: string, i: string, l: string){
+        this.title = t
+        this.price = p
+        this.image = i
+        this.link = l
+    }
+
+}
